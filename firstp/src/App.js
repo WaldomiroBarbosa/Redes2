8cad5b681c7d5c0ab752e4402be9bac7a8a4a6c6
Waldomiro Barbosa Romão <devwaldomirobarbosa@gmail.com>
2023-11-30 16:27:33 -0300

    First commit - Project

---
+++ firstp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Register from './componentes/Register'
+
+function App () 
+{
+
+    return (
+      <div>
+        <Register />
+      </div>
+
+    );
+
+  
+}
+
+export default App;
---
+++ firstp/src/componentes/Register.js
@@ -0,0 +1,103 @@
+import React, { useRef, useState, useEffect }  from 'react'
+
+const USER_REGEX = /^[0-9]{10}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
+function Register ()
+{
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setValidName(USER_REGEX.test(user));
+    }, [user])
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd));
+        setValidMatch(pwd === matchPwd);
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+
+    return (
+        <section style = {{textAlign: "center"}}>
+            { /* <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p> */ }
+            <h1>Registrar</h1>
+
+            <label htmlFor="username">
+                Username:
+
+            </label> 
+            <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+            />
+       
+       <label htmlFor="password">
+            Password:
+
+        </label>
+        <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+            aria-invalid={validPwd ? "false" : "true"}
+            aria-describedby="pwdnote"
+            onFocus={() => setPwdFocus(true)}
+            onBlur={() => setPwdFocus(false)}
+        />
+
+        <label htmlFor="confirm_pwd">
+            Confirm Password:
+
+        </label>
+        <input
+            type="password"
+            id="confirm_pwd"
+            onChange={(e) => setMatchPwd(e.target.value)}
+            value={matchPwd}
+            required
+            aria-invalid={validMatch ? "false" : "true"}
+            aria-describedby="confirmnote"
+            onFocus={() => setMatchFocus(true)}
+            onBlur={() => setMatchFocus(false)}
+        />
+
+        </section>
+    );
+}
+
+export default Register
