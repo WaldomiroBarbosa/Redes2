5b8eee0370ec89e881a3877fd43f15e7ed748a1f
Diogo Conceição Nandes <136838420+DiogoCNandes@users.noreply.github.com>
2023-12-12 15:34:01 -0300

    Update index.js

--- RoomProject/server/index.js
+++ RoomProject/server/index.js
@@ -42,3 +42,40 @@ app.post('/register', (req, res)=>
         }
     })
 })
+// Rota para verificar o status da sala
+app.get('/room/status/:studentId', (req, res) => {
+    const studentId = req.params.studentId;
+
+    // Execute a consulta no banco de dados para obter o status da sala com o ID do estudante fornecido
+    db.query('SELECT status FROM rooms WHERE studentId = ?', [studentId], (err, result) => {
+        if (err) {
+            console.error('Erro ao obter status da sala:', err);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+        } else {
+            if (result.length > 0) {
+                res.json({ status: result[0].status });
+            } else {
+                res.status(404).json({ error: 'Sala não encontrada' });
+            }
+        }
+    });
+});
+
+// Rota para marcar a sala como liberada
+app.post('/room/free/:studentId', (req, res) => {
+    const studentId = req.params.studentId;
+
+    // Execute a consulta no banco de dados para atualizar o status da sala para 'free'
+    db.query('UPDATE rooms SET status = "free" WHERE studentId = ?', [studentId], (err, result) => {
+        if (err) {
+            console.error('Erro ao marcar a sala como liberada:', err);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+        } else {
+            if (result.affectedRows > 0) {
+                res.json({ message: 'Sala marcada como liberada' });
+            } else {
+                res.status(404).json({ error: 'Sala não encontrada' });
+            }
+        }
+    });
+});
